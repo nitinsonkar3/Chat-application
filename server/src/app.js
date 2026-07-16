decea952e3cda47700d26740b53e0847987b8c2e
nitinsonkar3 <nitinsonkar2004ss@gmail.com>
2026-07-16 13:19:49 +0530

    connecting the database

---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "🚀 Chat Application Backend Running",
+  });
+});
+
+export default app;
